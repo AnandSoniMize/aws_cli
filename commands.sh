ec2ef8bb210b67354929b4ee19aa02091cf252a7
Maxim Yefremov <maxYefr@gmail.com>
2018-05-07 10:53:52 +0300

    configure list

--- commands.sh
+++ commands.sh
@@ -10,5 +10,14 @@ create_ec2(){
 	aws ec2 run-instances --image-id ami-916f59f4 --count 1 --instance-type t2.micro #--key-name MyKeyPair --security-groups my-sg
 
 }
+configure_list(){
+	aws configure list
+#      Name                    Value             Type    Location
+#      ----                    -----             ----    --------
+#   profile                <not set>             None    None
+#access_key     ******************* shared-credentials-file
+#secret_key     ******************* shared-credentials-file
+#    region             eu-central-1      config-file    ~/.aws/config
 
+}
 $@
