57e9f36468fbd4570bc27c042023b28a14070b6d
Maxim Yefremov <maxYefr@gmail.com>
2018-05-07 11:14:20 +0300

    find image

--- commands.sh
+++ commands.sh
@@ -7,7 +7,7 @@ createSecurityGroup(){
 }
 create_ec2(){
 	# https://docs.aws.amazon.com/cli/latest/userguide/cli-ec2-launch.html
-	aws ec2 run-instances --image-id ami-916f59f4 --count 1 --instance-type t2.micro #--key-name MyKeyPair --security-groups my-sg
+	aws ec2 run-instances --image-id ami-415b7baa --count 1 --instance-type t2.micro #--key-name MyKeyPair --security-groups my-sg
 
 }
 configure_list(){
@@ -19,5 +19,10 @@ configure_list(){
 #secret_key     ******************* shared-credentials-file
 #    region             eu-central-1      config-file    ~/.aws/config
 
+}
+find_image(){
+	# https://gist.github.com/vancluever/7676b4dafa97826ef0e9
+	aws ec2 describe-images  --filters Name=name,Values=ubuntu/images/hvm-ssd/ubuntu*  --query 'Images[*].[ImageId,CreationDate]' --output text  | sort -k2 -r  | head -n1
+	# ami-415b7baa	2018-05-01T17:34:04.000Z
 }
 $@
